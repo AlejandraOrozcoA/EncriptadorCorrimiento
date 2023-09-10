487d2b94f5b5bc470fb3ce331e8414bca5ef059b
Alejandra Orozco Aguilar <ale12orozco@gmail.com>
2023-09-09 21:57:27 -0600

    Se agrego el descifrado

--- assets/js/leerDoc.js
+++ assets/js/leerDoc.js
@@ -1,5 +1,18 @@
 let nombreArchivo;
 
+//Obtiene el valor del select "opciones" para saber si se va a cifrar o descifrar
+function getOpcion(){
+  var opcion = document.getElementById("opciones").value;
+  if (opcion == 1) {
+    return 1; //Cifrar
+  } else if (opcion == 2) {
+    return 2; //Descifrar
+  } else{
+    return 0;
+  }
+}
+
+//Abre el archivo de txt y obtiene el contenido
 function leerArchivo() {
   let doc = document.getElementById('file-song')
   let archivo = doc.files[0];
@@ -11,16 +24,16 @@ function leerArchivo() {
   lector.onload = function(e) {
     var contenido = e.target.result;
     mostrarContenidoOriginal(contenido);
-    cifrado(contenido);
+    if (getOpcion() == 1) { //Cifrado
+      cifrado(contenido);
+    } else if (getOpcion() == 2) { //Descifrado
+      descifrado(contenido);
+    }
   };
   lector.readAsText(archivo);
 }
 
-function mostrarContenidoOriginal(contenido) {
-  var elemento = document.getElementById('original');
-  elemento.innerHTML = contenido;
-}
-
+//Cifra el texto del archivo haciendo el corrimiento
 function cifrado(contenido){
   //quita espacio en blanco, tabulaciones, saltos de linea y caracteres especiales
   var texto = contenido.replace(/\s+/g, '');
@@ -35,18 +48,50 @@ function cifrado(contenido){
   var corrimiento = parseInt(document.getElementById('corrimiento').value);
   var textoCifrado = "";
   for (let i = 0; i < texto.length; i++) {
-    if (texto[i].charCodeAt(0) >= 65 && texto[i].charCodeAt(0) <= 90) {
+    if (texto[i].charCodeAt(0) >= 65 && texto[i].charCodeAt(0) <= 87) {
       textoCifrado += String.fromCharCode((texto[i].charCodeAt(0) - 65 + corrimiento) % 26 + 65);
+    }else if (texto[i].charCodeAt(0) >= 88 && texto[i].charCodeAt(0) <= 90) {
+      textoCifrado += String.fromCharCode((texto[i].charCodeAt(0) - 65 + corrimiento - 26) % 26 + 65);
     }
   }
   mostrarContenidoCifrado(textoCifrado);
 }
 
+//Descifra el texto del archivo haciendo el corrimiento inverso
+function descifrado(contenido){
+  //quita espacio en blanco, tabulaciones, saltos de linea y caracteres especiales
+  var texto = contenido.replace(/\s+/g, '');
+  texto = texto.replace(/[^a-zA-Z ]/g, "");
+  //convierte a mayusculas usando el codigo ascii
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i].charCodeAt(0) >= 97 && texto[i].charCodeAt(0) <= 122) {
+      texto = texto.replace(texto[i], String.fromCharCode(texto[i].charCodeAt(0) - 32));
+    }
+  }
+  //Hace el corrimiento (descifrado)
+  var corrimiento = parseInt(document.getElementById('corrimiento').value);
+  var textoDescifrado = "";
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i].charCodeAt(0) >= 68 && texto[i].charCodeAt(0) <= 90) {
+      textoDescifrado += String.fromCharCode((texto[i].charCodeAt(0) - 65 - corrimiento) % 26 + 65);
+    }else if (texto[i].charCodeAt(0) >= 65 && texto[i].charCodeAt(0) <= 67) {
+      textoDescifrado += String.fromCharCode((texto[i].charCodeAt(0) - 65 - corrimiento + 26) % 26 + 65);
+    }
+  }
+  mostrarContenidoCifrado(textoDescifrado);
+}
+
+// Funciones para mostrar el contenido en pantalla
+function mostrarContenidoOriginal(contenido) {
+  var elemento = document.getElementById('original');
+  elemento.innerHTML = contenido;
+}
 function mostrarContenidoCifrado(contenido) {
   var elemento = document.getElementById('cifrado');
   elemento.innerHTML = contenido;
 }
 
+//Crear archivo de texto y descargarlo
 function descargarArchivo() {
   var contenido = document.getElementById('cifrado')
   //crea un archivo de texto con el contenido cifrado
@@ -70,14 +115,3 @@ function descargarArchivo() {
   }, 0);
 }
 
-//Obtiene el valor del select "opciones"
-function getOpcion(){
-  var opcion = document.getElementById("opciones").value;
-  if (opcion == 1) {
-    return 1; //Cifrar
-  } else if (opcion == 2) {
-    return 2; //Descifrar
-  } else{
-    return 0;
-  }
-}
