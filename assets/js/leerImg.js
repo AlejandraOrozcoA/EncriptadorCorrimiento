208e10df302b1741bb5cc3bd75ec119960c6bf1a
Alejandra Orozco Aguilar <ale12orozco@gmail.com>
2023-09-13 20:23:46 -0600

    Cifra y descarga la imagen

--- assets/js/leerImg.js
+++ assets/js/leerImg.js
@@ -36,19 +36,18 @@ function leerImagen() {
       cifrar();
     }
     else if (opcion == 2) {
-      //descifrar();
+      descifrar();
     }   
   }
 
-//mostrar la imagen en el canvas
+//Cifra la imagen
 function cifrar() {
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   var img = new Image();
-  img.src = nombreImg;
+  img.src = "img/"+nombreImg;
   img.crossOrigin = "Anonymous";
   img.onload = function(){
-    console.log("hola");
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.drawImage(img,0,0);
@@ -56,7 +55,6 @@ function cifrar() {
     const imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
     const pixeles = imageData.data;
     console.log(pixeles.length);
-    var x = 0;
     for(var i = 0; i < pixeles.length; i+=4){
       r = pixeles[i];
       g = pixeles[i+1];
@@ -76,21 +74,74 @@ function cifrar() {
       ctx.putImageData(imageData, 0, 0);
     }
   }
+  crearBotonIMG();
+}
+
+//Descifra la imagen
+function descifrar() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var img = new Image();
+  img.src = "img/"+nombreImg;
+  img.crossOrigin = "Anonymous";
+  img.onload = function(){
+    ctx.drawImage(img,0,0);
+    var corrimiento = document.getElementById("corrimiento").value;
+    const imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+    const pixeles = imageData.data;
+    console.log(pixeles.length);
+    for(var i = 0; i < pixeles.length; i+=4){
+      r = pixeles[i];
+      g = pixeles[i+1];
+      b = pixeles[i+2];
+
+      r = Math.max((r - corrimiento) % 256 , 0, 0) ;
+      g = Math.max(0, (g - corrimiento) % 256, 0) ;
+      b = Math.max(0, 0, (b - corrimiento) % 256) ;
+
+      //pintarlo en el canvas
+      pixeles[i] = r;
+      pixeles[i+1] = g;
+      pixeles[i+2] = b;
+      pixeles[i+3] = 255;
+
+      //actualizar el canvas
+      ctx.putImageData(imageData, 0, 0);
+    }
+  }
+  crearBotonIMG();
 }
 
 // Funciones para mostrar el contenido en pantalla
 function mostrarImgOriginal() {
     var imagen = document.getElementById('img-original');
     var url = "img/"+nombreImg;
-    imagen.setAttribute("src", nombreImg );
+    imagen.setAttribute("src", url );
     
   }
 
 //Crea el boton para descargar el archivo
-function crearBoton(){
+function crearBotonIMG(){
     var boton = document.createElement("button");
-    boton.innerHTML = "Descargar";
+    boton.innerHTML = "Descargar Imagen";
     boton.setAttribute("class", "btn btn-primary");
-    boton.setAttribute("onclick", "descargarArchivo()");
+    boton.setAttribute("onclick", "descargarImagen()");
     document.getElementById("div-c-d").appendChild(boton);
+  }
+
+  //Descarga la imagen
+  function descargarImagen(){
+    var canvas = document.getElementById("canvas");
+    var link = document.createElement('a');
+    //Obtener opcion
+    var opcion = getOpcion();
+    if (opcion == 1) {
+      var nom = nombreImg.split(".")[0] + "_C.bmp";
+      link.download = nom;
+    } else if (opcion == 2) {
+      var nom = nombreImg.split(".")[0] + "_C.bmp";
+      link.download = nom;
+    } 
+    link.href = canvas.toDataURL()
+    link.click();
   }
